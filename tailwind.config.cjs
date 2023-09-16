7eef5633411bad3f79b46bd3d255f4ea224feb84
Htoo Khant <vexixongaming02@gmail.com>
2023-09-16 16:50:42 +0630

    rebug

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,36 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     "./node_modules/flowbite/**/*.js"
+//     // "node_modules/daisyui/dist/**/*.js",
+//     // "node_modules/react-daisyui/dist/**/*.js",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         base: "#B19777",
+//         secondary: "#535353",
+//         transparent: "transparent",
+//         current: "currentColor",
+//       },
+//       backgroundImage: {
+//         "login-bg": "url('/assets/bg.png')",
+//       },
+//     },
+//   },
+//   plugins: [require( "flowbite/plugin","daisyui")],
+//   colors: {
+//     primary: "#B19777",
+//   },
+// };
+/**   @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
-    // "node_modules/daisyui/dist/**/*.js",
-    // "node_modules/react-daisyui/dist/**/*.js",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
@@ -20,7 +45,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require( "flowbite/plugin","daisyui")],
+  plugins: [require("daisyui", "flowbite/plugin")],
   colors: {
     primary: "#B19777",
   },
