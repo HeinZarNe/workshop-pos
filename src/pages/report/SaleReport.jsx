5e4245b61410d055adff3cf53fe1aa9f7ecf96d6
Htoo Khant <vexixongaming02@gmail.com>
2023-10-01 01:22:39 +0630

    h

--- src/pages/report/SaleReport.jsx
+++ src/pages/report/SaleReport.jsx
@@ -10,33 +10,19 @@ import { Chart } from "primereact/chart";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import DonutChart from "./DonutChart";
 import BarChart from "./BarChart";
-import {
-  useGetBrandReportQuery,
-  useGetSaleReportQuery,
-} from "../../services/authApi";
+import { useGetBrandReportQuery } from "../../services/authApi";
 import { useTodaySaleQuery, useWeeklySaleQuery } from "../../services/authApi";
-import { Pagination } from "@mantine/core";
 
 const SaleReport = () => {
-  // const [view, setView] = useState("list");
-  // const [showSidebar, setShowSidebar] = useState(false);
-  // const [stockData, setStockData] = useState({});
-  // const [keyword, setKeyword] = useState("");
-  // const [addStock, setAddStock] = useState(false);
-  const [page, setPage] = useState("");
-  const [totalPage, setTotalPage] = useState(0);
-  // other
-  const token = localStorage.getItem("token");
   const [chartData, setChartData] = useState({});
   const [chartOptions, setChartOptions] = useState({});
   const [urlLink, setUrlLink] = useState("weekly-report");
-  const { data: saleReport, refetch } = useGetSaleReportQuery({ token, page });
-  console.log(saleReport?.data);
-  // console.log(urlLink);
+  console.log(urlLink);
   // const [click, setClick] = useState(false);
+  const token = localStorage.getItem("token");
   const { data: todaySale } = useTodaySaleQuery(token);
   const { data: sale } = useWeeklySaleQuery({ token, urlLink });
-  // console.log(sale);
+  console.log(sale);
 
   const { data: ch } = useGetBrandReportQuery(localStorage.getItem("token"));
   const maxPercent = (
@@ -47,52 +33,92 @@ const SaleReport = () => {
     (todaySale?.todayMinSales?.total * 100) /
     todaySale?.todayTotalSales
   ).toFixed(2);
-
-  // const [editstate, setEditState] = useState(false);
-  // useEffect(() => {
-  //   refetch();
-  //   return () => {};
-  // }, [addStock]);
-  useEffect(() => {
-    setTotalPage(saleReport?.meta?.last_page);
-    return () => {};
-  }, [saleReport]);
+  const tableData = [
+    {
+      id: 1,
+      no: 3,
+      name: "acer",
+      brand: "acer",
+      sale: 1000,
+      btn: (
+        <button className="flex items-center justify-center w-7 h-7 rounded-full bg-base text-black">
+          <AiOutlineArrowRight />
+        </button>
+      ),
+    },
+    {
+      id: 2,
+      no: 3,
+      name: "acer",
+      brand: "acer",
+      sale: 1000,
+      btn: (
+        <button className="flex items-center justify-center w-7 h-7 rounded-full bg-base text-black">
+          <AiOutlineArrowRight />
+        </button>
+      ),
+    },
+    {
+      id: 3,
+      no: 3,
+      name: "acer",
+      brand: "acer",
+      sale: 1000,
+      btn: (
+        <button className="flex items-center justify-center w-7 h-7 rounded-full bg-base text-black">
+          <AiOutlineArrowRight />
+        </button>
+      ),
+    },
+    {
+      id: 4,
+      no: 3,
+      name: "acer",
+      brand: "acer",
+      sale: 1000,
+      btn: (
+        <button className="flex items-center justify-center w-7 h-7 rounded-full bg-base text-black">
+          <AiOutlineArrowRight />
+        </button>
+      ),
+    },
+  ];
 
   // barchart
   useEffect(() => {
     if (urlLink === "weekly-report" && sale?.weeklySales) {
       // useEffect(() => {
-        const mon = sale?.weeklySales[0]?.daySales;
-        const tue = sale?.weeklySales[1]?.daySales;
-        const wed = sale?.weeklySales[2]?.daySales;
-        const thu = sale?.weeklySales[3]?.daySales;
-        const fri = sale?.weeklySales[4]?.daySales;
-        const sat = sale?.weeklySales[5]?.daySales;
-        const sun = sale?.weeklySales[6]?.daySales;
-        console.log(mon);
+      const mon = sale?.weeklySales[0]?.daySales;
+      const tue = sale?.weeklySales[1]?.daySales;
+      const wed = sale?.weeklySales[2]?.daySales;
+      const thu = sale?.weeklySales[3]?.daySales;
+      const fri = sale?.weeklySales[4]?.daySales;
+      const sat = sale?.weeklySales[5]?.daySales;
+      const sun = sale?.weeklySales[6]?.daySales;
+      console.log(mon);
 
-        const data = {
-          labels: ["S", "M", "T", "W", "T", "F", "S"],
-          datasets: [
-            {
-              data: [sun, mon, tue, wed, thu, fri, sat],
-              backgroundColor: ["#B19177"],
-              label: "Sales",
-            },
-          ],
-        };
-        const options = {
-          maintainAspectRatio: true,
-          aspectRatio: 1.3,
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
+      const data = {
+        labels: ["S", "M", "T", "W", "T", "F", "S"],
+        datasets: [
+          {
+            data: [sun, mon, tue, wed, thu, fri, sat],
+            backgroundColor: ["#B19177"],
+            label: "Sales",
           },
-        };
-        console.log(data, options);
-        setChartData(data);
-        setChartOptions(options);
+        ],
+      };
+      const options = {
+        maintainAspectRatio: true,
+        aspectRatio: 1.3,
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      };
+      console.log(data, options);
+      setChartData(data);
+      setChartOptions(options);
       // }, []);
     } else if (urlLink === "monthly-report" && sale?.monthlySales) {
       if (sale) {
@@ -495,7 +521,7 @@ const SaleReport = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    {saleReport?.data?.map((data) => {
+                    {tableData?.map((data) => {
                       return (
                         <tr
                           key={data.id}
@@ -505,21 +531,17 @@ const SaleReport = () => {
                             scope="row"
                             className="px-6 py-4 font-medium text-white whitespace-nowra"
                           >
-                            {data.id}
+                            {data.no}
                           </th>
                           <td className="px-6 py-4">{data.name}</td>
-                          <td className="px-6 py-4">{data.brand_name}</td>
-                          <td className="px-6 py-4  text-end">
-                            {data.sale_price}
-                          </td>
+                          <td className="px-6 py-4">{data.brand}</td>
+                          <td className="px-6 py-4  text-end">{data.sale}</td>
                           <td className="px-6 py-4 text-right">
                             <NavLink
                               to={"/profile"}
                               className="font-medium flex ps-5 justify-center text-blue-600  hover:underline"
                             >
-                              <button className="flex items-center justify-center w-7 h-7 rounded-full bg-base text-black">
-                                <AiOutlineArrowRight />
-                              </button>
+                              {data.btn}
                             </NavLink>
                           </td>
                         </tr>
@@ -556,23 +578,6 @@ const SaleReport = () => {
               </div>
             </div>
           </div>
-          <Pagination
-            total={totalPage || 1}
-            onChange={(e) => {
-              setPage(e);
-              refetch();
-            }}
-            // onPreviousPage={(e) => {
-            //   setPage((prev) => prev > 0 && prev - 1);
-            //   refetch();
-            // }}
-            // onNextPage={(e) => {
-
-            //   refetch();
-            // }}
-            boundaries={1}
-            defaultValue={1}
-          />
         </div>
       </div>
     </Rootlayout>
